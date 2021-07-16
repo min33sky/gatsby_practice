6aea23b77c02991e80876b7adfe1d620a0539f1b
min33sky <min33sky@naver.com>
2021-07-16 11:13:48 +0900

    add: comment

---
+++ src/components/Post/CommentWidget.tsx
@@ -0,0 +1,44 @@
+import React, { createRef, FC, useEffect } from 'react';
+
+const src = 'https://utteranc.es/client.js';
+const repo = 'min33sky/min33sky.github.io';
+
+type UtterancesAttributesType = {
+  src: string;
+  repo: string;
+  'issue-term': string;
+  label: string;
+  theme: string;
+  crossorigin: string;
+  async: string;
+};
+
+const CommentWidget: FC = () => {
+  const element = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (element.current === null) return;
+
+    const utterances: HTMLScriptElement = document.createElement('script');
+
+    const attributes: UtterancesAttributesType = {
+      src,
+      repo,
+      'issue-term': 'pathname',
+      label: 'Comment',
+      theme: 'github-light',
+      crossorigin: 'anonymouse',
+      async: 'true',
+    };
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value);
+    });
+
+    element.current.appendChild(utterances);
+  }, []);
+
+  return <div ref={element} />;
+};
+
+export default CommentWidget;
--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,4 +1,5 @@
 import Template from 'components/Common/Template';
+import CommentWidget from 'components/Post/CommentWidget';
 import PostContent from 'components/Post/PostContent';
 import PostHead, { PostHeadProps } from 'components/Post/PostHead';
 import { graphql } from 'gatsby';
@@ -32,6 +33,7 @@ const PostTemplate: FC<PostTemplateProps> = ({
     <Template>
       <PostHead {...frontmatter} />
       <PostContent html={html} />
+      <CommentWidget />
     </Template>
   );
 };
